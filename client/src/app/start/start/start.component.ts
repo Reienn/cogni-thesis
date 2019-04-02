18a434b96631739611eae346a741a13bcafab0e7
Anna Jankowicz <anna.maria.jankowicz@gmail.com>
2019-04-03 00:31:55 +0200

    UI start page layout, bg image

--- client/src/app/start/start/start.component.ts
+++ client/src/app/start/start/start.component.ts
@@ -12,19 +12,24 @@ export class StartComponent implements OnInit {
   msg: string;
 
   showAbout = false;
+  showSignup = false;
+  showLogin = false;
 
   constructor(private authGuardService: AuthGuardService) { }
 
   ngOnInit() {
     if (this.authGuardService.getErr()) {
       this.err = this.authGuardService.getErr();
-      this.selectedAuthForm = 1;
+      // this.selectedAuthForm = 1;
+      this.showLogin = true;
     }
   }
 
   redirectToLogin(msg) {
     this.msg = msg;
-    this.selectedAuthForm = 1;
+    // this.selectedAuthForm = 1;
+    this.showSignup = false;
+    this.showLogin = true;
 
   }
 
