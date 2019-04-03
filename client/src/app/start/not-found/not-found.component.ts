727db591eeebfa2f699bd59e90333f6c266367f4
Anna Jankowicz <anna.maria.jankowicz@gmail.com>
2019-04-03 17:19:02 +0200

    UI error page refresh link

--- client/src/app/start/not-found/not-found.component.ts
+++ client/src/app/start/not-found/not-found.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
   template: `
     <div class="error-container"><div class="error-msg">
       <h2><i class="fa fa-exclamation-triangle"></i> Błąd</h2>Strona nie istnieje
-      <a mat-flat-button color="primary" [routerLink]="['/']"><i class="fa fa-arrow-circle-left"></i> Powrót</a>
+      <a mat-flat-button color="primary" href="/"><i class="fa fa-arrow-circle-left"></i> Powrót</a>
     </div></div>
   `
 })
