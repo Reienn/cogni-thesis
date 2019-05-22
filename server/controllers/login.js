e03543cf0693c0be507ddf59fde016751078a021
Anna Jankowicz <anna.maria.jankowicz@gmail.com>
2019-05-22 19:04:35 +0200

    JWT config var

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -2,6 +2,7 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
+const SECRET_KEY = process.env.JWT_KEY ? process.env.JWT_KEY : require('../settings.conf.json').jwt.key;
 
 module.exports.login = function(req, res, next) {
   if(!req.body) { res.send(400).send('Brak danych'); }
@@ -9,7 +10,7 @@ module.exports.login = function(req, res, next) {
   User.findOne({name: req.body.name}, (err, user) => {
     if(err) return next(err);
     if(user && bcrypt.compareSync(req.body.psw, user.psw)) {
-      jwt.sign({name: user.name, group: user.group, currentCase: user.currentCase}, 'secretkey', {expiresIn: '7d'}, (err, token) => {
+      jwt.sign({name: user.name, group: user.group, currentCase: user.currentCase}, SECRET_KEY, {expiresIn: '7d'}, (err, token) => {
         if(err) return next(err);
         res.json({token: token,
                   group: user.group,
--- server/services/jwt.js
+++ server/services/jwt.js
@@ -1,10 +1,11 @@
 'use strict';
 const jwt = require('jsonwebtoken');
+const SECRET_KEY = process.env.JWT_KEY ? process.env.JWT_KEY : require('../settings.conf.json').jwt.key;
 
 module.exports.verifyToken = function(req, res, next) {
   try {
     const token = req.headers.authorization.split(' ')[1];
-    req.userData = jwt.verify(token, 'secretkey');
+    req.userData = jwt.verify(token, SECRET_KEY);
     next();
   } catch (error) {
     res.status(400).send({error: 'Token verification failed'});
